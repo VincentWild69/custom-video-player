c11b8c26d06d3fd799380c9dc57bbe06cf3fbb14
Alexey Kuzniecov <alexeyk90@gmail.com>
2021-09-02 16:16:44 +0300

    add app to gh

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # custom-video-player
- my custom video player
+
+https://vincentwild69.github.io/custom-video-player/index.html
+
+**my try to create custom video player**
+
+*implemented all the basic functions required for a video player
+*hokeys available
+*responsive
+
+_! in demo only first have sound, last three videos without sound !_
+
+_enjoy :)_
---
+++ script.js
@@ -0,0 +1,327 @@
+const flexWrapper = document.querySelector('.flex-wrapper');
+const videoContainer = document.querySelector('.video-container');
+const controls = document.querySelector('.controls');
+const player = document.querySelector('.player');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const playBtn = document.querySelector('.play-btn');
+const times = document.querySelector('.times');
+const curTime = document.querySelector('.cur-time');
+const durTime = document.querySelector('.dur-time');
+const progressBar = document.querySelector('.progress-bar');
+const muteBtn = document.querySelector('.mute-btn');
+const volumeBar = document.querySelector('.volume-bar');
+const fsBtn = document.querySelector('.fs-btn');
+const bigBtn = document.querySelector('.video-big-btn');
+
+const videos = ['assets/video/video1.mp4', 'assets/video/Robin-21723.mp4', 'assets/video/Rose-3654.mp4', 'assets/video/Star - 6962.mp4'];
+
+let activeVideoIndex = 0;
+let volLevel = 0; //save volume level when click 'mute off' or change it to 0% on keboard
+
+
+nextBtn.addEventListener('click', nextVideo);
+prevBtn.addEventListener('click', prevVideo);
+
+playBtn.addEventListener('click', videoAction);
+player.addEventListener('click', videoAction);
+bigBtn.addEventListener('click', videoAction);
+
+player.addEventListener('timeupdate', updatePosition);
+
+player.addEventListener('loadedmetadata', () => {
+  durTime.innerText = convertTime(player.duration);
+})
+player.addEventListener('ended', () => {
+  playBtn.style.backgroundImage = 'url(assets/controls/play.svg)'
+});
+
+progressBar.addEventListener('input', function() {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`
+  player.currentTime = player.duration * this.value / 100;
+});
+
+volumeBar.addEventListener('input', function() {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`;
+  changeVolume();
+});
+
+muteBtn.addEventListener('click', mute);
+
+fsBtn.addEventListener('click', toggleFullScreen);
+player.addEventListener('dblclick', toggleFullScreen);
+
+// меняем стили плеера при наличии режима фулскрин
+document.addEventListener('fullscreenchange', () => {
+  if (!document.fullscreenElement) {
+      videoContainer.classList.remove('video-container-fs');
+      player.classList.remove('video-fs');
+      controls.classList.remove('controls-fs');
+  } else {
+    videoContainer.classList.add('video-container-fs');
+    player.classList.add('video-fs');
+    controls.classList.add('controls-fs');
+  }
+  hideControls();
+  }
+)
+
+//костыль: нужен чтобы при первой загрузке сразу отображалось время первого видео
+function videoDuration() {
+  let a = player.play();
+  durTime.innerText = convertTime(player.duration || 0);
+  if (a !== undefined) {
+    a.then(_ => {
+      setTimeout(player.pause(), 0);
+    })
+    .catch(error => {});
+  };
+}
+videoDuration();
+
+//hot-keys
+document.addEventListener('keydown', (event) => {
+  let keyName = event.code;
+  if (!event.repeat) { //чтобы при зажатой клавише событие не срабатывало бесконечно
+    switch (keyName) { 
+    // case 'Escape':
+    //   event.preventDefault();
+    //   break;
+    case 'KeyF':
+      event.preventDefault();
+      toggleFullScreen();
+      break;
+    case 'Space':
+      event.preventDefault();
+      videoAction();
+      break;
+    case 'KeyM':
+      event.preventDefault();
+      mute();
+      break;
+    case 'Period':
+      event.preventDefault();
+      speedPlus();
+      break;
+    case 'Comma':
+      event.preventDefault();
+      speedMinus();
+      break;
+    case 'ArrowLeft':
+      event.preventDefault();
+      if (event.ctrlKey) {
+        prevVideo();
+      } else {
+        if (player.currentTime - 5 > 0) {
+          player.currentTime -= 5;
+        } else {
+          player.currentTime = 0;
+        }
+      }
+      break;
+    case 'ArrowRight':
+      event.preventDefault();
+      if (event.ctrlKey) {
+        nextVideo();
+      } else {
+        if (player.currentTime + 5 < player.duration) {
+          player.currentTime += 5;
+        } else {
+          player.currentTime = player.duration;
+        }
+      }
+      break;
+    case 'ArrowUp':
+      event.preventDefault();
+      volumePlus();
+      break;
+    case 'ArrowDown':
+      event.preventDefault();
+      if (player.volume > 0) { //чтобы нажатие клавиши в режиме мут не сбивало запомненный уровень громкости в ноль
+        volumeMinus();
+      }
+      break;
+    default:
+      break; 
+    }
+  }
+})
+
+// comboKeys(nextVideo, 'ControlLeft', 'ArrowRight');
+// comboKeys(prevVideo, 'ControlLeft', 'ArrowLeft');
+
+//functions
+function videoAction() {
+  if (player.paused) {
+    player.play();
+    playBtn.style.backgroundImage = 'url(assets/controls/pause.svg)';
+    bigBtn.classList.add('dn');
+  } else {
+    player.pause();
+    playBtn.style.backgroundImage = 'url(assets/controls/play.svg)';
+  }
+}
+
+function nextVideo() {
+  activeVideoIndex++
+  if (activeVideoIndex == videos.length) {
+    activeVideoIndex = 0;
+  }
+  player.setAttribute('src', videos[activeVideoIndex]);
+  playBtn.style.backgroundImage = 'url(assets/controls/play.svg)';
+  bigBtn.classList.remove('dn');
+}
+
+function prevVideo() {
+  activeVideoIndex--
+  if (activeVideoIndex < 0) {
+    activeVideoIndex = videos.length - 1;
+  }
+  player.setAttribute('src', videos[activeVideoIndex]);
+  playBtn.style.backgroundImage = 'url(assets/controls/play.svg)';
+  bigBtn.classList.remove('dn');
+}
+
+function convertTime(sec) {
+  let mm = Math.floor(sec / 60);
+  let ss = Math.floor(sec - mm * 60);
+  if (mm < 10) {
+    mm = '0' + mm;
+  }
+  if (ss < 10) {
+    ss = '0' + ss;
+  }
+  return `${mm}:${ss}`;
+}
+
+function updatePosition() {
+  curTime.innerText = convertTime(player.currentTime || 0);
+  durTime.innerText = convertTime(player.duration || 0);
+  progressBar.value = (((player.currentTime || 0) / (player.duration || 1)) * 100);
+  progressBar.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${progressBar.value}%, #C4C4C4 ${progressBar.value}%, #C4C4C4 100%)`;
+}
+
+function changeVolume() {
+  let volume = volumeBar.value / 100;
+  player.volume = volume;
+  if (player.volume == 0) {
+    muteBtn.style.backgroundImage = `url(assets/controls/muteoff.svg)`;
+    volLevel = 0;
+  }
+  else {
+    muteBtn.style.backgroundImage = `url(assets/controls/mute.svg)`;
+  }
+};
+
+function mute() {
+  if (player.volume == 0) {
+    player.volume = volLevel;
+    volumeBar.value = volLevel * 100;
+    if (volLevel > 0) {
+      muteBtn.style.backgroundImage = `url(assets/controls/mute.svg)`;
+    }
+    volumeBar.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${volumeBar.value}%, #C4C4C4 ${volumeBar.value}%, #C4C4C4 100%)`;
+  } else {
+    volLevel = player.volume
+    player.volume = 0;
+    volumeBar.value = 0;
+    muteBtn.style.backgroundImage = `url(assets/controls/muteoff.svg)`;
+    volumeBar.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${volumeBar.value}%, #C4C4C4 ${volumeBar.value}%, #C4C4C4 100%)`;
+  }
+}
+
+function toggleFullScreen() {
+  if (!document.fullscreenElement) {
+    flexWrapper.requestFullscreen();
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+  }
+}
+
+function speedPlus() {
+  if (player.playbackRate < 16) {
+    player.playbackRate = (player.playbackRate + 0.1).toFixed(1);
+  }
+}
+
+function speedMinus() {
+  if (player.playbackRate > 0) {
+    player.playbackRate = (player.playbackRate - 0.1).toFixed(1);
+  }
+}
+
+function volumePlus() {
+  if (player.volume < 1) {
+    player.volume = (player.volume + 0.1).toFixed(1);
+    volumeBar.value = player.volume * 100;
+    volumeBar.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${volumeBar.value}%, #C4C4C4 ${volumeBar.value}%, #C4C4C4 100%)`;
+  }
+  if (player.volume > 0) {
+    muteBtn.style.backgroundImage = `url(assets/controls/mute.svg)`;
+  }
+}
+
+function volumeMinus() {
+  if (player.volume > 0) {
+    player.volume = (player.volume - 0.1).toFixed(1);
+    volumeBar.value = player.volume * 100;
+    volumeBar.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${volumeBar.value}%, #C4C4C4 ${volumeBar.value}%, #C4C4C4 100%)`;
+  }
+  if (player.volume == 0) {
+    volLevel = 0;
+    muteBtn.style.backgroundImage = `url(assets/controls/muteoff.svg)`;
+  }
+}
+
+function hideControls() { //функция для скрытия контрольной панели в режиме фуллскрин
+  let timerControlsHide;
+  resetTimer();
+
+  document.addEventListener('mousemove', resetTimer);
+  document.addEventListener('keydown', resetTimer);
+
+  function controlsDN() {
+    controls.classList.add('dn');
+  }
+
+  function resetTimer() {
+
+    if (controls.classList.contains('dn')) {
+      controls.classList.remove('dn');
+      }
+
+    if (!document.fullscreenElement) { // убираем слушатели в режине НЕфулскрин
+      document.removeEventListener('mousemove', resetTimer);
+      document.removeEventListener('keydown', resetTimer);
+      clearTimeout(timerControlsHide);
+      return;
+    }
+
+    clearTimeout(timerControlsHide);
+    timerControlsHide = setTimeout(controlsDN, 2000)
+  }
+}
+
+// функция для работы сочетания клавиш
+// function comboKeys(func, ...codes) {
+//   let pressed = new Set();
+
+//   document.addEventListener('keydown', function(event) {
+//     pressed.add(event.code);
+//     for (let code of codes) {
+//       if (!pressed.has(code)) {
+//         return;
+//       }
+//     }
+//     pressed.clear();
+//     func();
+//   });
+
+//   document.addEventListener('keyup', function(event) {
+//     pressed.delete(event.code);
+//   });
+// }
